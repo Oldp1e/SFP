4235fa516a3e0ce435f6fc540a345ebc77e16809
Samuel Jr. <samuellima06091999@gmail.com>
2026-01-20 20:24:12 -0300

    initialize Spring Boot application with Maven wrapper and basic configuration

---
+++ src/test/java/com/oldp1e/sfp/SfpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oldp1e.sfp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
